1df60626cceac79c4965e30fd8a0a8a138da5e89
Enerccio WIN7 <admin@en-circle.com>
2017-12-31 12:59:21 +0100

    @kozec

---
+++ PythonTest/Assets/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.Scripting.Hosting;
+
+public class Test : MonoBehaviour {
+
+
+
+	void Start () {
+		ScriptEngine scriptEngine = IronPython.Hosting.Python.CreateEngine();  
+		ScriptScope scriptScope = scriptEngine.CreateScope();  
+		scriptEngine.Runtime.LoadAssembly(typeof(GameObject).Assembly);
+
+		TextAsset ta = Resources.Load<TextAsset>("MyScript");
+		ScriptSource src = scriptEngine.CreateScriptSourceFromString (ta.text);
+		src.Execute (scriptScope);
+		var printme = scriptScope.GetVariable<Microsoft.Scripting.Utils.Func<object, object>>("printme");
+		printme.Invoke (gameObject);
+		scriptScope.GetVariable<Microsoft.Scripting.Utils.Func<object, object>> ("deleteme").Invoke (gameObject);
+	}
+
+}
